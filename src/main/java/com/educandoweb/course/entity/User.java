b0bc63e5aed53f514808222279379fdd6c74bf22
dancesar <danillo.yamagush@gmail.com>
2020-01-06 14:12:43 -0300

    Arquivo properties

--- src/main/java/com/educandoweb/course/entity/User.java
+++ src/main/java/com/educandoweb/course/entity/User.java
@@ -7,12 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @Entity
 public class User implements Serializable {
 
